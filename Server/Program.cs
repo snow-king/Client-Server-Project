6b90a4632069ff336067bc25703ce9b16f0f196e
Sekfiser <66331553+Sekfiser@users.noreply.github.com>
2022-01-23 01:15:28 +0800

    Add files via upload

---
+++ Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: отдельный обект Runner для запуска исолнения и более удобного оборачивания в отдельный поток
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    /// <summary>
+    /// Отдельный класс для запуска потока исполнения
+    /// </summary>
+    class Runner 
+    {
+        /// <summary>
+        /// Метод старта основной программы
+        /// </summary>
+        public void Run() 
+        {
+            // TODO: вывести инфомрация из бд по нажатию любой кнопки
+        }
+    }
+}
---
+++ Server/SQLConnector.cs
@@ -0,0 +1,30 @@
+﻿using MySqlConnector;
+
+namespace Server
+{
+    /// <summary>
+    /// Класс для создания подключения и исполнения запросов
+    /// </summary>
+    public class SQLConnector
+    {
+        public MySqlConnection Connection { get; private set; }
+        private string ConnectionPath = ""; // TODO: Получение пути подключения из файла
+
+        /// <summary>
+        /// Конструктор, при создании объекта будет проверена возможность осуществить подключение
+        /// </summary>
+        public SQLConnector() 
+        {
+            Connection = null;
+        }
+        /// <summary>
+        /// Метод получеющий всю таблицу Timetable
+        /// </summary>
+        /// <returns>Возвращает строку с полученной информацией</returns>
+        public string SelectTimeTable() 
+        {
+
+            return null;
+        }
+    }
+}
