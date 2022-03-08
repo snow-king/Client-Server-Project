3c53b51bb3c80d95f2538a45daf40ca4b4b20ce5
Sekfiser <66331553+Sekfiser@users.noreply.github.com>
2022-03-09 02:27:16 +0800

    Add files via upload

---
+++ Client/Config/ConfigInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace Client
+{
+    public class ConfigInfo
+    {
+        public string UrlString() 
+        {
+            return ConfigurationManager.AppSettings.Get("Url");
+        }
+    }
+}
