a2ccc1aaf6fd9f38f549e4f39c7a21f4342c206a
YukinoKingu <59993603+snow-king@users.noreply.github.com>
2022-03-22 19:06:06 +0800

    Update Program.cs
    fix programm and comment

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Client;
-public class Program
-{
-    public static void Main(string[] args)
+using System; //(•ิ_•ิ)? (•ิ_•ิ)? (•ิ_•ิ)?
+using System.IO; //(•ิ_•ิ)? (•ิ_•ิ)? (•ิ_•ิ)?
+using System.Net; //(•ิ_•ิ)? (•ิ_•ิ)? (•ิ_•ิ)?
+using System.Threading.Tasks;
+
+namespace Server
+{    
+    class Program
     {
-        Runner runner = new Runner();
-        runner.Run();
+        static async Task Main(string[] args)
+        {
+            // TODO: отдельный обект Runner для запуска исолнения и более удобного оборачивания в отдельный поток
+            Runner runner = new Runner();
+            runner.Run();            
+
+            await Listener.Listen(runner.Con);
+        }
+    }
+
+    /// <summary>
+    /// Отдельный класс для запуска потока исполнения
+    /// </summary>
+    class Runner 
+    {        
+        public SQLConnector Con { get; private set; }
+        /// <summary>
+        /// Метод старта основной программы
+        /// </summary>
+        public void Run() 
+        {
+            Con = new SQLConnector();            
+        }
     }
 }
         
